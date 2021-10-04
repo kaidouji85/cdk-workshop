b0db46f75c9648b6b55677cb33ee805445a2605c
竹内佑介 <kaidouji85@gmail.com>
2021-10-04 18:26:26 +0900

    VPCを追加した

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,18 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+      ]
+    });
   }
 }
